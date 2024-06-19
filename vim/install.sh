760f55cb64e848a4cfffdc6d43c9b085239144dc
Nitish Nayak <felarof.nayak@gmail.com>
2024-06-18 23:40:53 -0400

    clean up vim folder

--- vim/install.sh
+++ vim/install.sh
@@ -6,6 +6,10 @@ function install_vimutils {
         echo "Installing Vundle"
         # install Vundle for vimrc
         git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+        echo "Getting my colorscheme"
+        wget https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim
+        mkdir -p "$HOME"/.vim/colors/
+        mv badwolf.vim "$HOME"/.vim/colors
     fi
 }
 
