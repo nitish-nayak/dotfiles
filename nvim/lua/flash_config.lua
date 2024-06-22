0a2c915c8873173596b5355fc63c43898fe6d4e8
Nitish Nayak <felarof.nayak@gmail.com>
2024-06-22 16:39:24 -0400

    some changes to cmp

--- nvim/lua/flash_config.lua
+++ nvim/lua/flash_config.lua
@@ -3,7 +3,7 @@ local flash = require("flash")
 flash.setup({
   modes = {
     char = {
-      jump_labels = true
+      jump_labels = false
     },
   },
   label = {
@@ -18,5 +18,5 @@ local opts = { noremap=true }
 vim.keymap.set({'n', 'x', 'o'}, 's', function() flash.jump() end, opts)
 vim.keymap.set({'n', 'x', 'o'}, '<leader>s', function() flash.treesitter() end, opts)
 vim.keymap.set({'o'}, 'r', function() flash.remote() end, opts)
-vim.keymap.set({'x', 'o'}, 'a', function() flash.treesitter_search() end, opts)
+vim.keymap.set({'x', 'o'}, 'R', function() flash.treesitter_search() end, opts)
 vim.keymap.set({'c'}, '<c-s>', function() flash.toggle() end, opts)
